89946e4736e981f2da12f7f7ef41a7ad718f45cc
박수빈 <87634136+subinbing@users.noreply.github.com>
2023-08-17 09:53:26 +0900

    Add files via upload

---
+++ 2935_소음.py
@@ -0,0 +1,12 @@
+num_1 = int(input())
+symbols = input()
+num_2 = int(input())
+
+if symbols == "+":
+    result = num_1 + num_2
+    print(result)
+
+elif symbols == "*":
+    result = num_1 * num_2
+    print(result)
+
